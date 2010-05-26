409d47604b387a571df3e5c2b02a15db54285968
Alan Broun <abroun@alanbroun.net>
2010-05-26 16:21:06 +0100

    Reorganised the UweSubPlugin a bit so that we can start using threaded drivers. Also cleaned up the Python scripts a bit so that they all use the same configuration files

--- scripts/SubController.py
+++ scripts/SubController.py
@@ -14,45 +14,7 @@ import Profiling
 
 #from ColourTracker import ColourTracker
 from RoBoardControl import ColourTracker
-
-#-------------------------------------------------------------------------------
-class SubControllerConfig( yaml.YAMLObject ):
-
-    yaml_tag = u'!SubControllerConfig'
-
-    ROBOT_TYPE_REAL = "Real"
-    ROBOT_TYPE_SIM = "Sim"
-
-    PLAYER_SERVER_ADDRESS = 'localhost'
-    PLAYER_SERVER_PORT = 6665
-    
-    TRACKED_HUE = 22.0
-    MAX_ABS_HUE_DIFF = 6.0
-    TRACKED_SATURATION = 70.0
-    MAX_ABS_SATURATION_DIFF = 5.0
-    TRACKED_VALUE = 60.0
-    MAX_ABS_VALUE_DIFF = 15.0
-
-    FORWARD_SPEED = 1.0
-    YAW_SPEED = 0.3
-    SCREEN_RADIUS_OF_CLOSE_BUOY = 0.2
-    
-    def __init__( self ):
-        self.robotType = self.ROBOT_TYPE_REAL
-
-        self.playerServerAddress = self.PLAYER_SERVER_ADDRESS
-        self.playerServerPort = self.PLAYER_SERVER_PORT
-
-        self.trackedHue = self.TRACKED_HUE
-        self.maxAbsHueDiff = self.MAX_ABS_HUE_DIFF
-        self.trackedSaturation = self.TRACKED_SATURATION
-        self.maxAbsSaturationDiff = self.MAX_ABS_SATURATION_DIFF
-        self.trackedValue = self.TRACKED_VALUE
-        self.maxAbsValueDiff = self.MAX_ABS_VALUE_DIFF
-
-        self.forwardSpeed = self.FORWARD_SPEED
-        self.yawSpeed = self.YAW_SPEED
-        self.screenRadiusOfCloseBuoy = self.SCREEN_RADIUS_OF_CLOSE_BUOY
+from SubControllerConfig import SubControllerConfig
 
 #-------------------------------------------------------------------------------
 class SubController:
