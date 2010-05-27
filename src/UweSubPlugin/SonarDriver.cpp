ef88b55b6f816bfe0df2b982e701442cb4ef5bba
Alan Broun <abroun@alanbroun.net>
2010-05-27 15:44:48 +0100

    Work on the skeleton sonar dirver

--- src/UweSubPlugin/SonarDriver.cpp
+++ src/UweSubPlugin/SonarDriver.cpp
@@ -7,6 +7,137 @@
 #include <stdio.h>
 #include "SonarDriver.h"
 
+#include <assert.h> // For RollingBuffer
+
+//------------------------------------------------------------------------------
+// RollingBuffer
+//------------------------------------------------------------------------------
+RollingBuffer::RollingBuffer( U32 bufferSize )
+{
+    mpBuffer = (U8*)malloc( bufferSize );
+    assert( NULL != mpBuffer && "Unable to allocate buffer" );
+    
+    mBufferSize = bufferSize;
+    mDataStartIdx = 0;
+    mDataSize = 0;
+}
+
+//------------------------------------------------------------------------------
+RollingBuffer::~RollingBuffer()
+{
+    if ( NULL != mpBuffer )
+    {
+        free( mpBuffer );
+        mpBuffer = NULL;
+    }
+}
+    
+//------------------------------------------------------------------------------
+void RollingBuffer::Clear()
+{
+    mDataStartIdx = 0;
+    mDataSize = 0;
+}
+    
+//------------------------------------------------------------------------------
+U32 RollingBuffer::ReadBytes( U8* pDataOut, U32 numBytes )
+{
+    U32 numBytesRead = PeekAtBytes( pDataOut, numBytes );
+    AdvanceBuffer( numBytesRead );
+    
+    return numBytesRead;
+}
+    
+//------------------------------------------------------------------------------
+U32 RollingBuffer::PeekAtBytes( U8* pDataOut, U32 numBytes ) const
+{
+    assert( NULL != pDataOut );
+    
+    U32 numBytesRead = numBytes;
+    if ( numBytesRead > mDataSize )
+    {
+        numBytesRead = mDataSize;
+    }
+    
+    // Test to see if we can do the read all in one go
+    U32 numContiguousBytesAvailable = mBufferSize - mDataStartIdx;
+    if ( numContiguousBytesAvailable >= numBytesRead )
+    {
+        memcpy( pDataOut, &mpBuffer[ mDataStartIdx ], numBytesRead );
+    }
+    else
+    {
+        // The read has to be done in 2 parts
+        memcpy( pDataOut, &mpBuffer[ mDataStartIdx ], numContiguousBytesAvailable );
+        
+        U32 numBytesRemaining = numBytesRead - numContiguousBytesAvailable;
+        memcpy( &pDataOut[ numContiguousBytesAvailable ],
+                &mpBuffer[ 0 ], numBytesRemaining );
+    }
+    
+    return numBytesRead;
+    
+}
+    
+//------------------------------------------------------------------------------
+void RollingBuffer::AdvanceBuffer( U32 numBytes )
+{
+    U32 bytesToAdvance = numBytes;
+    if ( bytesToAdvance > mDataSize )
+    {
+        bytesToAdvance = mDataSize;
+    }
+    
+    mDataStartIdx = AddOffsetToIdx( mDataStartIdx, bytesToAdvance );
+    mDataSize -= bytesToAdvance;
+}
+    
+//------------------------------------------------------------------------------
+bool RollingBuffer::TryToAddBytes( U8* pData, U32 numBytes )
+{
+    bool bBytesAdded = false;
+    
+    if ( numBytes < GetFreeSpace() )
+    {
+        // Test to see if we can write the data all in one go
+        U32 endOfDataIdx = AddOffsetToIdx( mDataStartIdx, mDataSize );
+        U32 numContiguousBytesAvailable = mBufferSize - endOfDataIdx;
+        if ( numContiguousBytesAvailable >= numBytes )
+        {
+            memcpy( &mpBuffer[ endOfDataIdx ], pData, numBytes );
+        }
+        else
+        {
+            // The write has to be done in 2 parts
+            memcpy( &mpBuffer[ endOfDataIdx ], pData, numContiguousBytesAvailable );
+            
+            U32 numBytesRemaining = numBytes - numContiguousBytesAvailable;
+            memcpy( &mpBuffer[ 0 ], 
+                    &pData[ numContiguousBytesAvailable ], numBytesRemaining );
+        }
+        
+        mDataSize += numBytes;
+        bBytesAdded = true;
+    }
+    
+    return bBytesAdded;
+}
+
+//------------------------------------------------------------------------------
+U32 RollingBuffer::AddOffsetToIdx( U32 idx, U32 offset ) const
+{
+    U32 numBytesToEnd = mBufferSize - idx;
+    if ( offset < numBytesToEnd )
+    {
+        return idx + offset;
+    }
+    else
+    {
+        // Wrap around and start from the beginning of the buffer
+        return offset - numBytesToEnd;
+    }
+}
+
 //------------------------------------------------------------------------------
 // A factory creation function, declared outside of the class so that it
 // can be invoked without any object context (alternatively, you can
--- src/UweSubPlugin/SonarDriver.h
+++ src/UweSubPlugin/SonarDriver.h
@@ -30,6 +30,47 @@
 #include "Common.h"
 
 //------------------------------------------------------------------------------
+class RollingBuffer
+{
+    // Constructor/Destructor
+    public: RollingBuffer( U32 bufferSize );
+    public: ~RollingBuffer();
+    
+    // Information about the buffer
+    public: U32 GetSize() const { return mBufferSize; }
+    public: U32 GetNumBytesInBuffer() const { return mDataSize; }
+    public: U32 GetFreeSpace() const { return mBufferSize - mDataSize; }
+    
+    // Clears all data from the buffer
+    public: void Clear();
+    
+    // Reads data from the buffer and advances the read pointer.
+    // Returns the number of bytes that were actually read.
+    public: U32 ReadBytes( U8* pDataOut, U32 numBytes );
+    
+    // Similar to ReadBytes but doesn't advance the buffer
+    // Returns the number of bytes that were actually read.
+    public: U32 PeekAtBytes( U8* pDataOut, U32 numBytes ) const;
+    
+    // Advances the read pointer by at most numBytes
+    public: void AdvanceBuffer( U32 numBytes );
+    
+    // Attempts to add numBytes of data to the buffer. If there isn't
+    // enough space in the buffer then it returns false. Otherwise it
+    // returns true.
+    public: bool TryToAddBytes( U8* pData, U32 numBytes );
+    
+    // Helper routine for making sure that indicies stay within the buffer
+    private: U32 AddOffsetToIdx( U32 idx, U32 offset ) const;
+    
+    // Member variables
+    private: U8* mpBuffer;
+    private: U32 mBufferSize;
+    private: U32 mDataStartIdx;
+    private: U32 mDataSize;
+};
+
+//------------------------------------------------------------------------------
 class SonarDriver : public ThreadedDriver
 {
     typedef ThreadedDriver base;
