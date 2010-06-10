b2d76c00baac90c46474cabca51f3ed28d5af0b7
uwesub <uwesub@uwesub-desktop.(none)>
2010-06-10 20:23:53 +0100

    Basic compass functionality

--- src/UweSubPlugin/CompassDriver.cpp
+++ src/UweSubPlugin/CompassDriver.cpp
@@ -6,6 +6,7 @@
 //------------------------------------------------------------------------------
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 #include "CompassDriver.h"
 
 //------------------------------------------------------------------------------
@@ -30,9 +31,29 @@ void CompassDriverRegister( DriverTable* pTable )
 }
 
 //------------------------------------------------------------------------------
+// Helper routines for reading data types from a buffer. These assume that we're 
+// reading big-endian data into little-endian data types
+static inline U16 READ_U16( U8* buffer )
+{
+    return buffer[ 0 ] << 8 | buffer[ 1 ];
+}
+
+static inline U32 READ_U32( U8* buffer )
+{
+    return buffer[ 0 ] << 24 | buffer[ 1 ] << 16 | buffer[ 2 ] << 8 | buffer[ 3 ];
+}
+
+static inline F32 READ_F32( U8* buffer )
+{
+    U32 floatData = READ_U32( buffer );
+    return *((F32*)&floatData);
+}
+
+//------------------------------------------------------------------------------
 // CompassDriver
 //------------------------------------------------------------------------------
 const U32 CompassDriver::DEFAULT_BUFFER_SIZE = 10000;
+const S32 CompassDriver::COMPASS_COMMAND_ID_PACKET_IDX = 2;
 
 //------------------------------------------------------------------------------
 // Constructor.  Retrieve options from the configuration file and do any
@@ -40,7 +61,9 @@ const U32 CompassDriver::DEFAULT_BUFFER_SIZE = 10000;
 CompassDriver::CompassDriver( ConfigFile* pConfigFile, int section )
     : ThreadedDriver( pConfigFile, section, false, 
         PLAYER_MSGQUEUE_DEFAULT_MAXLEN, PLAYER_IMU_CODE ),
-    mBufferSize( "buffer_size", DEFAULT_BUFFER_SIZE, false )
+    mBufferSize( "buffer_size", DEFAULT_BUFFER_SIZE, false ),
+    mState( eS_Invalid ),
+    mExpectedResponse( eCC_None )
 {
     this->alwayson = true;
     
@@ -69,7 +92,7 @@ CompassDriver::~CompassDriver()
 //------------------------------------------------------------------------------
 // Set up the device.  Return 0 if things go well, and -1 otherwise.
 int CompassDriver::MainSetup()
-{
+{    
     PLAYER_WARN( "Setting up Compass driver" );
 
     if ( Device::MatchDeviceAddress( mOpaqueID, this->device_addr ) )
@@ -91,20 +114,9 @@ int CompassDriver::MainSetup()
         return -1;
     }
 
-    // Code to send a message
-    //uint8_t* buffer = new uint8_t[MESSAGE_LEN];
-    //assert(buffer);
-    //makeStartStopCommand(buffer, true);
-    //player_opaque_data_t mData;
-    //mData.data_count = MESSAGE_LEN;
-    //mData.data = buffer;
-
-    //mpOpaque->PutMsg( this->InQueue, 
-    //    PLAYER_MSGTYPE_CMD, PLAYER_OPAQUE_CMD_DATA, 
-    //    reinterpret_cast<void*>(&mData), 0, NULL );
-
-    //delete [] buffer;
-    //buffer = NULL;
+    // Start by getting information about the compass
+    SendGetModInfoCommand();
+    mState = eS_GettingModInfo;
 
     PLAYER_WARN( "Compass driver ready" );
 
@@ -126,12 +138,12 @@ void CompassDriver::MainQuit()
 // Process all messages for this driver.
 int CompassDriver::ProcessMessage( QueuePointer& respQueue,
                                 player_msghdr* pHeader, void* pData )
-{   
+{    
     if ( Message::MatchMessage(
         pHeader, PLAYER_MSGTYPE_DATA, PLAYER_OPAQUE_DATA_STATE, mOpaqueID ) )
     {
         player_opaque_data_t* pOpaqueData = (player_opaque_data_t*)pData;
-        
+                
         if ( pOpaqueData->data_count <= mBuffer.GetFreeSpace() )
         {
             mBuffer.TryToAddBytes( pOpaqueData->data, pOpaqueData->data_count );
@@ -155,9 +167,9 @@ void CompassDriver::Main()
     {
         // Wait for messages to arrive
         base::Wait();
-        
+
         base::ProcessMessages();
-        
+
         ProcessData();
     }
 }
@@ -165,20 +177,287 @@ void CompassDriver::Main()
 //------------------------------------------------------------------------------
 void CompassDriver::ProcessData()
 {
-    const S32 MAX_STRING_LENGTH = 1024;
-    U8 stringBuffer[ MAX_STRING_LENGTH + 1 ];
+    const S32 MIN_PACKET_LENGTH = 5;    // U16 Byte count + U8 id + U16 crc
+    const S32 MAX_PACKET_LENGTH = 1024;
+    U8 packetBuffer[ MAX_PACKET_LENGTH ];
+    U16 numBytesInPacket = 0;
+    bool bPacketReady = false;
     
-    while ( mBuffer.GetNumBytesInBuffer() > 0 )
+    eCompassCommand packetCommand = eCC_None;
+    
+    // First see if there is a packet available to be read from the buffer
+    if ( mBuffer.GetNumBytesInBuffer() >= sizeof( U16 ) )
     {
-        U32 numBytesToRead = mBuffer.GetNumBytesInBuffer();
-        if ( numBytesToRead > MAX_STRING_LENGTH )
+        // We have at least enough bytes for the byte count
+        U8 byteCountBuffer[ sizeof( U16 ) ];
+        mBuffer.PeekAtBytes( byteCountBuffer, sizeof( U16 ) );
+        numBytesInPacket = READ_U16( byteCountBuffer );
+        
+        if ( mBuffer.GetNumBytesInBuffer() >= numBytesInPacket )
         {
-            numBytesToRead = MAX_STRING_LENGTH;
+            assert( numBytesInPacket <= MAX_PACKET_LENGTH && "MAX_PACKET_LENGTH needs to be increased" );
+            mBuffer.ReadBytes( packetBuffer, numBytesInPacket );
+            
+            // Check the packet for errors
+            if ( numBytesInPacket >= MIN_PACKET_LENGTH )
+            {
+                // Get the command id and CRC from the packet
+                packetCommand = (eCompassCommand)packetBuffer[ COMPASS_COMMAND_ID_PACKET_IDX ];
+                U16 packetCRC = READ_U16( &packetBuffer[ numBytesInPacket - 2 ] );
+                
+                if ( CalculateCRC( packetBuffer, numBytesInPacket - 2 ) == packetCRC )
+                {                
+                    bPacketReady = true;
+                    if ( eCC_None != mExpectedResponse )
+                    {
+                        if ( mExpectedResponse == packetCommand )
+                        {
+                            // We have recieved the response we're waiting for
+                            mExpectedResponse = eCC_None;
+                        }
+                        else
+                        {
+                            fprintf( stderr, "Error: Unexpected response packet recieved\n" );
+                            bPacketReady = false;   // Throw the invalid packet away
+                        }
+                    }
+                }
+                else
+                {
+                    fprintf( stderr, "Error: Packet with invalid CRC recieved\n" );
+                }
+            }
+            else
+            {
+                fprintf( stderr, "Error: Undersized packet recieved\n" );
+            }
+        }
+    }
+        
+    // Now process any packet which we've received
+    if ( bPacketReady )
+    {
+        switch ( mState )
+        {
+            case eS_GettingModInfo:
+            {
+                assert( eCC_ModInfoResp == packetCommand );
+
+                U8 typeBuffer[ 5 ];
+                memcpy( typeBuffer, &packetBuffer[ 3 ], 4 );
+                typeBuffer[ 4 ] = '\0';
+                
+                U8 revisionBuffer[ 5 ];
+                memcpy( revisionBuffer, &packetBuffer[ 7 ], 4 );
+                revisionBuffer[ 4 ] = '\0';
+                
+                printf( "Attached to PNI compass\n" );
+                printf( "Compass Type = %s, Revision = %s\n", typeBuffer, revisionBuffer );
+                
+                SendGetDataCommand();
+                mState = eS_PollingForData;
+                
+                break;
+            }
+            case eS_PollingForData:
+            {
+                assert( eCC_DataResp == packetCommand );
+                CompassData compassData;
+                ExtractDataFromPacket( packetBuffer, numBytesInPacket, &compassData );
+                
+                // Publish the data
+                player_imu_data_euler publishData;
+                memset( &publishData, 0, sizeof( player_imu_data_euler ) );
+                
+                publishData.calib_data.magn_x = compassData.mAlignedMagneticX;
+                publishData.calib_data.magn_y = compassData.mAlignedMagneticY;
+                publishData.calib_data.magn_z = compassData.mAlignedMagneticZ;
+                publishData.orientation.proll = compassData.mRollDegrees*M_PI/180.0f;
+                publishData.orientation.ppitch = compassData.mPitchDegrees*M_PI/180.0f;
+                publishData.orientation.pyaw = compassData.mHeadingDegrees*M_PI/180.0f;
+                
+                base::Publish( this->device_addr, 
+                    PLAYER_MSGTYPE_DATA, PLAYER_IMU_DATA_EULER, 
+                    &publishData, sizeof( publishData ) );
+                
+                //printf( "H: %2.3f, P: %2.3f, R: %2.3f\n", 
+                //    compassData.mHeadingDegrees, compassData.mRollDegrees, compassData.mRollDegrees );
+                    
+                SendGetDataCommand();
+                break;
+            }
+            default:
+            {
+                assert( false && "Invalid state encountered" );
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+void CompassDriver::SendGetModInfoCommand()
+{
+    U8 buffer[] =
+    {
+        0x00, 0x05,     // Num bytes
+        0x01,           // Command id
+        0xEF, 0xD4      // CRC
+    };
+    
+    SendCommandDataBuffer( buffer, sizeof( buffer ), eCC_ModInfoResp );
+}
+
+//------------------------------------------------------------------------------
+void CompassDriver::SendGetDataCommand()
+{
+    U8 buffer[] =
+    {
+        0x00, 0x05,     // Num bytes
+        0x04,           // Command id
+        0xBF, 0x71      // CRC
+    };
+    
+    SendCommandDataBuffer( buffer, sizeof( buffer ), eCC_DataResp );
+}
+
+//------------------------------------------------------------------------------
+void CompassDriver::SendCommandDataBuffer( void* pData, S32 numBytes, 
+                                           eCompassCommand expectedResponse )
+{
+    assert( eCC_None == mExpectedResponse 
+        && "Trying to send a command when we're already waiting for a response" );
+    
+    player_opaque_data_t dataMsg;
+    dataMsg.data_count = numBytes;
+    dataMsg.data = (U8*)pData;
+    
+    mpOpaque->PutMsg( this->InQueue, 
+        PLAYER_MSGTYPE_CMD, PLAYER_OPAQUE_CMD_DATA, 
+        (U8*)&dataMsg, 0, NULL );
+        
+    mExpectedResponse = expectedResponse;
+}
+
+//------------------------------------------------------------------------------
+void CompassDriver::ExtractDataFromPacket( U8* pPacketBuffer, S32 numBytes, 
+                                           CompassData* pCompassDataOut )
+{
+    assert( eCC_DataResp == pPacketBuffer[ COMPASS_COMMAND_ID_PACKET_IDX ] && "Invalid packet type" );
+    
+    // Clear out the compass data
+    memset( pCompassDataOut, 0, sizeof( CompassData ) );
+    
+    // Look at the payload of the packet (Ignore the byte count, packet id and CRC)
+    U8* pDataBuffer = &pPacketBuffer[ COMPASS_COMMAND_ID_PACKET_IDX + 1 ];
+    U8* pDataBufferEnd = &pPacketBuffer[ numBytes - 2 ];    
+    
+    U8 numComponents = *pDataBuffer;
+    pDataBuffer++;
+    
+    while ( pDataBuffer < pDataBufferEnd )
+    {
+        eCompassDataComponent dataComponent = (eCompassDataComponent)(*pDataBuffer);
+        pDataBuffer++;
+        switch ( dataComponent )
+        {
+            case eCDC_HeadingDegrees:
+            {
+                pCompassDataOut->mHeadingDegrees = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+           case eCDC_TemperatureCelsius:
+            {
+                pCompassDataOut->mTemperatureCelsius = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_Distortion:
+            {
+                pCompassDataOut->mbDistortion = (bool)pDataBuffer[ 0 ];
+                pDataBuffer++;
+                break;
+            }
+            case eCDC_Calibrated:
+            {
+                pCompassDataOut->mbCalibrated = (bool)pDataBuffer[ 0 ];
+                pDataBuffer++;
+                break;
+            }    
+            case eCDC_PitchCalibratedG:
+            {
+                pCompassDataOut->mPitchCalibratedG = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_RollCalibratedG:
+            {
+                pCompassDataOut->mRollCalibratedG = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_IZ_CalibratedG:
+            {
+                pCompassDataOut->mIZ_CalibratedG = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_PitchDegrees:
+            {
+                pCompassDataOut->mPitchDegrees = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_RollDegrees:
+            {
+                pCompassDataOut->mRollDegrees = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_AlignedMagneticX:
+            {
+                pCompassDataOut->mAlignedMagneticX = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_AlignedMagneticY:
+            {
+                pCompassDataOut->mAlignedMagneticY = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            case eCDC_AlignedMagneticZ:
+            {
+                pCompassDataOut->mAlignedMagneticZ = READ_F32( pDataBuffer );
+                pDataBuffer += sizeof( F32 );
+                break;
+            }
+            default:
+            {
+                printf( "Unhandled Data component = %i, idx = %i\n", dataComponent, pDataBuffer - pPacketBuffer );
+                assert( false && "Unhandled data component encountered" );
+            }
         }
         
-        mBuffer.ReadBytes( stringBuffer, numBytesToRead );
-        stringBuffer[ numBytesToRead ] = '\0';
-        
-        printf( "%s", stringBuffer );
+        // TODO: It would be nice if this check was done before the read instead of after
+        assert( pDataBuffer <= pDataBufferEnd && "Read beyond end of buffer" );
     }
+}
+
+//------------------------------------------------------------------------------
+U16 CompassDriver::CalculateCRC( U8* pData, U32 numBytes )
+{
+    U32 index = 0;
+    U16 crc = 0;
+
+    while( index < numBytes )
+    {
+        crc = (U8)(crc >> 8) | (crc << 8);
+        crc ^= pData[ index++ ];
+        crc ^= (U8)(crc & 0xff) >> 4;
+        crc ^= (crc << 8) << 4;
+        crc ^= ((crc & 0xff) << 4) << 1;
+    }
+    
+    return crc;
 }
--- src/UweSubPlugin/CompassDriver.h
+++ src/UweSubPlugin/CompassDriver.h
@@ -31,6 +31,82 @@
 #include "DataStructures/RollingBuffer.h"
 
 //------------------------------------------------------------------------------
+enum eCompassCommand
+{
+    eCC_None = 0,          // Set when we're not expecting a response
+    eCC_GetModInfo,        // Queries the modules type and firmware revision number.
+    eCC_ModInfoResp,       // Response to kGetModInfo
+    eCC_SetDataComponents, // Sets the data components to be output.
+    eCC_GetData,           // Queries the module for data
+    eCC_DataResp,          // Response to kGetData
+    eCC_SetConfig,         // Sets internal configurations in the module
+    eCC_GetConfig,         // Queries the module for the current internal configuration value
+    eCC_ConfigResp,        // Response to kGetConfig
+    eCC_Save,              // Commands the module to save internal and user calibration
+    eCC_StartCal,          // Commands the module to start user calibration
+    eCC_StopCal,           // Commands the module to stop user calibration
+    eCC_SetParam,          // Sets the FIR filter settings for the magnetometer & accelerometer
+                            // sensors.
+    eCC_GetParam,          // Queries for the FIR filter settings for the magnetometer &
+                            // accelerometer sensors.
+    eCC_ParamResp,         // Contains the FIR filter settings for the magnetometer &
+                            // accelerometer sensors.
+    eCC_PowerDown,         // Used to completely power-down the module
+    eCC_SaveDone,          // Response to kSave
+    eCC_UserCalSampCount,  // Sent from the module after taking a calibration sample point
+    eCC_UserCalScore,      // Contains the calibration score
+    eCC_SetConfigDone,     // Response to kSetConfig
+    eCC_SetParamDone,      // Response to kSetParam
+    eCC_StartIntervalMode, // Commands the module to output data at a fixed interval
+    eCC_StopIntervalMode,  // Commands the module to stop data output at a fixed interval
+    eCC_PowerUp,           // Sent after wake up from power down mode
+    eCC_SetAcqParams,      // Sets the sensor acquisition parameters
+    eCC_GetAcqParams,      // Queries for the sensor acquisition parameters
+    eCC_AcqParamsDone,     // Response to kSetAcqParams
+    eCC_AcqParamsResp,     // Response to kGetAcqParams
+    eCC_PowerDownDone,     // Response to kPowerDown
+    eCC_FactoryUserCal,    // Clears user calibration coefficients
+    eCC_FactorUserCalDone, // Response to kFactoryUserCal
+    eCC_TakeUserCalSample, // Commands the unit to take a sample during user calibration
+    eCC_NumCommands
+};
+
+enum eCompassDataComponent
+{
+    eCDC_HeadingDegrees = 5,
+    eCDC_TemperatureCelsius = 7,
+    eCDC_Distortion = 8,
+    eCDC_Calibrated = 9,
+    eCDC_PitchCalibratedG = 21,
+    eCDC_RollCalibratedG = 22,
+    eCDC_IZ_CalibratedG = 23,
+    eCDC_PitchDegrees = 24,
+    eCDC_RollDegrees = 25,
+    eCDC_AlignedMagneticX = 27,
+    eCDC_AlignedMagneticY = 28,
+    eCDC_AlignedMagneticZ = 29
+};
+
+//------------------------------------------------------------------------------
+// TODO: We might want to put in bools to say whether fields are valid or not.
+// For now we just set invalid fields to 0
+struct CompassData
+{
+    F32 mHeadingDegrees;
+    F32 mTemperatureCelsius;
+    bool mbDistortion;
+    bool mbCalibrated;
+    F32 mPitchCalibratedG;
+    F32 mRollCalibratedG;
+    F32 mIZ_CalibratedG;
+    F32 mPitchDegrees;
+    F32 mRollDegrees;
+    F32 mAlignedMagneticX;
+    F32 mAlignedMagneticY;
+    F32 mAlignedMagneticZ;
+};
+
+//------------------------------------------------------------------------------
 class CompassDriver : public ThreadedDriver
 {
     typedef ThreadedDriver base;
@@ -55,6 +131,15 @@ class CompassDriver : public ThreadedDriver
     
     private: void ProcessData();
     
+    private: void SendGetModInfoCommand();
+    private: void SendGetDataCommand();
+    private: void SendCommandDataBuffer( void* pData, S32 numBytes, eCompassCommand expectedResponse = eCC_None );
+    
+    private: void ExtractDataFromPacket( U8* pPacketBuffer, S32 numBytes, CompassData* pCompassDataOut );
+    
+    // Helper routine that calcuates the CRC value for a block of data
+    private: U16 CalculateCRC( U8* pData, U32 numBytes );
+
     // Properties
     private: IntProperty mBufferSize;
     
@@ -65,6 +150,18 @@ class CompassDriver : public ThreadedDriver
     private: RollingBuffer mBuffer;
     
     private: static const U32 DEFAULT_BUFFER_SIZE;
+    private: static const S32 COMPASS_COMMAND_ID_PACKET_IDX;
+    
+    private: enum eState
+    {
+        eS_Invalid,
+        eS_GettingModInfo,
+        eS_PollingForData,
+        eS_NumStates
+    };
+    
+    private: eState mState;
+    private: eCompassCommand mExpectedResponse;
 };
 
 //------------------------------------------------------------------------------
